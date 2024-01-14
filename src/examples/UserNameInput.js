c56b9cf88fed0aff54a0bc1b4daddbc16a1d8d3c
Venkat.R <ramsunvtech@gmail.com>
2024-01-14 21:03:14 +0800

    example: add for username input

---
+++ src/examples/UserNameInput.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { InputSingleLine } from './TwixtUI/react'
+
+function UserNameInput() {
+    const [value, setValue] = useState('');
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    const handleBlur = () => {
+        console.log('Input field lost focus');
+    };
+
+    return (
+        <>
+            <InputSingleLine
+                label="User Name"
+                value={value}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={value === '' ? 'Field cannot be empty' : ''}
+                warning={value.length > 0 && value.length < 5 ? 'Text is too short' : ''}
+            />
+        </>
+    )
+}
+
+export default UserNameInput
