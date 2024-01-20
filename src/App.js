d90491d67565066bc8b7e64ade201322cdb2b1b5
Venkat.R <ramsunvtech@gmail.com>
2024-01-20 12:23:15 +0800

    build: update latest

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { ToolName } from 'TwixtUI/react'
 
 // Components.
 import { ErrorHandler } from '@tw/components'
 import UserNameInput from '@tw/examples/UserNameInput'
+import Header from '@tw/examples/Header'
 
 function App() {
   return (
     <>
       <ErrorHandler>
-        <ToolName name="Dash" />
         <h1>Light React App with TwixtUI</h1>
         <div>
           <UserNameInput />
         </div>
+        <Header />
       </ErrorHandler>
     </>
   )
