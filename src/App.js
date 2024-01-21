f8391b59de97eb56db9cf8c4cdaa629cfe8b2a9e
Venkat.R <ramsunvtech@gmail.com>
2024-01-21 21:32:01 +0800

    app: import modal

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Components.
 import { ErrorHandler } from '@tw/components'
 import UserNameInput from '@tw/examples/UserNameInput'
 import Header from '@tw/examples/Header'
+import Modal from '@tw/examples/Modal'
 import { LeftSideBar, RightSideBar, ContentPane } from '@tw/examples/Skeleton'
 
 function App() {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <>
       <ErrorHandler>
-        <div class="flex h-screen bg-green-300">
-          <div class="flex-1 flex flex-col overflow-hidden">
+        {isOpen && <Modal />}
+        <div className="flex h-screen bg-green-300">
+          <div className="flex-1 flex flex-col overflow-hidden">
             <Header />
-            <div class="flex h-full">
+            <div className="flex h-full">
               <LeftSideBar />
               <ContentPane>
                 <UserNameInput />
+                {/* Open modal button */}
+                <button onClick={() => setOpen(true)} className="px-4 py-2 bg-indigo-500 text-white rounded-md">
+                  Open Modal
+                </button>
               </ContentPane>
               <RightSideBar />
             </div>
