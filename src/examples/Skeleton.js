f5b0b88ae51f911fcb861190306773849d4d39e2
Venkat.R <ramsunvtech@gmail.com>
2024-01-21 21:32:48 +0800

    update skeleton

--- src/examples/Skeleton.js
+++ src/examples/Skeleton.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 
 export function LeftSideBar() {
   return (
-    <nav class="flex w-72 h-full bg-pink-500">
-      <div class="w-full flex mx-auto px-6 py-8">
-        <div class="w-full h-full flex items-center justify-center text-gray-900 text-xl border-4 border-gray-900 border-dashed">Sidebar</div>
+    <nav className="flex w-72 h-full bg-pink-500">
+      <div className="w-full flex mx-auto px-6 py-8">
+        <div className="w-full h-full flex items-center justify-center text-gray-900 text-xl border-4 border-gray-900 border-dashed">Sidebar</div>
       </div>
     </nav>
   );
@@ -12,9 +12,9 @@ export function LeftSideBar() {
 
 export function RightSideBar() {
   return (
-    <nav class="flex w-72 h-full bg-yellow-400">
-      <div class="w-full flex mx-auto px-6 py-8">
-        <div class="w-full h-full flex items-center justify-center text-gray-900 text-xl border-4 border-gray-900 border-dashed">Rightbar</div>
+    <nav className="flex w-72 h-full bg-yellow-400">
+      <div className="w-full flex mx-auto px-6 py-8">
+        <div className="w-full h-full flex items-center justify-center text-gray-900 text-xl border-4 border-gray-900 border-dashed">Rightbar</div>
       </div>
     </nav>
   );
@@ -22,15 +22,15 @@ export function RightSideBar() {
 
 export function ContentPane({ children }) {
   return (
-    <main class="flex flex-col w-full bg-white overflow-x-hidden overflow-y-auto mb-14">
-      <div class="flex w-full mx-auto px-6 py-8">
-        <div class="flex flex-col w-full h-full text-gray-900 text-xl border-4 border-gray-900 border-dashed">
+    <main className="flex flex-col w-full bg-white overflow-x-hidden overflow-y-auto mb-14">
+      <div className="flex w-full mx-auto px-6 py-8">
+        <div className="flex flex-col w-full h-full text-gray-900 text-xl border-4 border-gray-900 border-dashed">
           {children}
-          <div class="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
-          <div class="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
-          <div class="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
-          <div class="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
-          <div class="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
+          <div className="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
+          <div className="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
+          <div className="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
+          <div className="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
+          <div className="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
         </div>
       </div>
     </main>
