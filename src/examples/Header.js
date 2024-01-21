8dd49e2c7ae014e8066b9f21659efcb12a344f9f
Venkat.R <ramsunvtech@gmail.com>
2024-01-21 20:28:39 +0800

    header: update button right padding

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -6,7 +6,7 @@ function TwixHeader({ children }) {
     <header class="flex justify-between items-center bg-blue-300 p-4">
       <div class="flex">
         <button
-          class="bg-slate-800 text-slate-100 rounded-full">
+          class="bg-slate-800 text-slate-100 rounded-full pr-2">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
             stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
