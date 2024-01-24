56578c0c35ee9c7eab6d0cd682b4dea96f69afce
Venkat.R <ramsunvtech@gmail.com>
2024-01-24 19:18:51 +0800

    app: externalized all components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { useState } from 'react'
 
-import { TwixtButton, TwixtMultiLineInput, TwixtSwitch, LeftSideBar, RightSideBar, ContentPane } from 'TwixtUI/react';
+import { TwixtCommentsSystem, TwixtLoadingPostCard, TwixtButton, TwixtMultiLineInput, TwixtSwitch, LeftSideBar, RightSideBar, ContentPane } from 'TwixtUI/react';
 
 // Components.
 import { ErrorHandler } from '@tw/components'
 import UserNameInput from '@tw/examples/UserNameInput'
-import Header from '@tw/examples/Header'
+import SampleAppHeader from '@tw/examples/Header'
 import Heading from '@tw/examples/Heading'
 import Modal from '@tw/examples/Modal'
-import TwixtSimpleTable from '@tw/examples/SimpleTable'
-import CommentsSystem from '@tw/examples/CommentsSystem'
-import LoadingPostCard from '@tw/examples/LoadingPostCard'
+import FruitsTable from '@tw/examples/SimpleTable'
 
 function App() {
   const [isOpen, setOpen] = useState(false);
@@ -21,11 +19,16 @@ function App() {
         {isOpen && <Modal onSubmit={() => setOpen(false)} />}
         <div className="flex h-screen bg-green-300">
           <div className="flex-1 flex flex-col overflow-hidden">
-            <Header />
+            <SampleAppHeader />
             <div className="flex h-full">
-              <LeftSideBar />
+              <LeftSideBar>
+                <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700">Home</a>
+                <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700">About</a>
+                <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700">Services</a>
+                <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700">Contact</a>
+              </LeftSideBar>
               <ContentPane>
-              <LoadingPostCard />
+                <TwixtLoadingPostCard />
                 <Heading />
                 <TwixtSwitch label="Toggle" onChange={status => {
                   console.log('status: ', status)
@@ -35,10 +38,11 @@ function App() {
                   Open Modal
                 </TwixtButton>
                 <TwixtMultiLineInput />
-                <TwixtSimpleTable />
-                <CommentsSystem />
+                <FruitsTable />
+                <TwixtCommentsSystem />
+                <div className="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
               </ContentPane>
-              <RightSideBar />
+              <RightSideBar>Rightbar</RightSideBar>
             </div>
           </div>
         </div>
