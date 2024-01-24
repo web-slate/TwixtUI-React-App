d93b170ca2acbc3ce7fdcdcc70cdd2cfa796a67a
Venkat.R <ramsunvtech@gmail.com>
2024-01-24 18:23:21 +0800

    header: moved header to twixt-ui

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,27 +1,11 @@
 import React, { useState } from 'react'
-import { ToolName } from 'TwixtUI/react'
-
-function TwixtHeader({ children }) {
-  return (
-    <header className="flex justify-between items-center bg-blue-300 p-4">
-      <div className="flex">
-        <button
-          className="bg-slate-800 text-slate-100 rounded-full pr-2">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-            stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinecap="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-        </button>
-        {children}
-      </div>
-      <div className="flex">Right</div>
-    </header>
-  );
-}
+import { TwixtHeader, ToolName } from 'TwixtUI/react'
 
 export default function Header() {
   return (
-    <TwixtHeader>
+    <TwixtHeader headerClass="bg-blue-300" rightBlock={(
+      <>Right</>
+    )}>
       <ToolName name="Twixt UI React App" />
     </TwixtHeader>
   );
