c1bfcaaeff05c41e2c7c11db90fc6496a7f5cff8
Venkat.R <ramsunvtech@gmail.com>
2024-01-24 18:30:21 +0800

    loading-post-card: clean up and use twixt

--- src/examples/LoadingPostCard.js
+++ src/examples/LoadingPostCard.js
@@ -1,20 +1,9 @@
 import React from 'react';
+import { TwixtLoadingPostCard } from 'TwixtUI/react'
 
 function LoadingPostCard() {
   return (
-    <div class="w-1/3">
-      <div class="max-w-sm rounded overflow-hidden shadow-lg animate-pulse">
-        <div class="h-48 bg-gray-300"></div>
-        <div class="px-6 py-4">
-          <div class="h-6 bg-gray-300 mb-2"></div>
-          <div class="h-4 bg-gray-300 w-2/3"></div>
-        </div>
-        <div class="px-6 pt-4 pb-2">
-          <div class="h-4 bg-gray-300 w-1/4 mb-2"></div>
-          <div class="h-4 bg-gray-300 w-1/2"></div>
-        </div>
-      </div>
-    </div>
+    <TwixtLoadingPostCard />
   )
 }
 
