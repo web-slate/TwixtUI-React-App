cb80cc1fc855a3605cd50ded27b9e8f556161310
Venkat.R <ramsunvtech@gmail.com>
2024-01-27 22:46:03 +0800

    util: add props data

---
+++ src/examples/data.js
@@ -0,0 +1,43 @@
+export const usersCardList = [{
+    title: 'Shankar Ganesh',
+    cardImageUrl: 'https://avatars.githubusercontent.com/u/3478542?v=4',
+    cardBackgroundUrl: 'images/banner.png',
+    subTitle: ['Associate Vice President', 'Coimbatore South, Tamil Nadu, India', 'Sleep Mode: On'],
+    stats: [
+        { label: 'Joined', value: '1st Sep 2020' },
+        { label: 'Total Points', value: '17,625' },
+        { label: 'Available Per Week', value: '3.5 hours' },
+    ]
+},
+{
+    title: 'Karthikeyan Annamalai',
+    cardImageUrl: 'https://avatars.githubusercontent.com/u/6830053?v=4',
+    cardBackgroundUrl: 'images/banner.png',
+    subTitle: ['Devops Lead', 'Chennai, Tamil Nadu, India', 'Sleep Mode: On'],
+    stats: [
+        { label: 'Joined', value: '1st Sep 2020' },
+        { label: 'Total Points', value: '36,655' },
+        { label: 'Available Per Week', value: '3 hours' },
+    ]
+}, {
+    title: 'Yuvaraj Ravi',
+    cardImageUrl: 'https://avatars.githubusercontent.com/u/29545937?v=4',
+    cardBackgroundUrl: 'images/banner.png',
+    subTitle: ['Associate Vice President', 'Bengaluru, Karnataka, India', 'Sleep Mode: Off'],
+    stats: [
+        { label: 'Joined', value: '1st Sep 2020' },
+        { label: 'Total Points', value: '21,845' },
+        { label: 'Available Per Week', value: '2.5 hours' },
+    ],
+}];
+
+export const statItems = [{
+    label: 'Transactions every 24 hours',
+    value: '44 million',
+}, {
+    label: 'Assets under holding',
+    value: '$119 trillion',
+}, {
+    label: 'New users annually',
+    value: '46,000',
+}];
