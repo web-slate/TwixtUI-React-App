17910564b932113189ce0e75f0634f27aac8c963
Venkat.R <ramsunvtech@gmail.com>
2024-01-27 15:41:44 +0800

    example: update header additional announcement

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -2,8 +2,13 @@ import React from 'react'
 import { TwixtHeader, TwixtToolName } from 'TwixtUI/react'
 
 export default function Header() {
+  const announceMent = {
+    title: 'title',
+    desc: 'desc goes here',
+  };
+
   return (
-    <TwixtHeader headerClass="bg-blue-300" rightBlock={(
+    <TwixtHeader announcment={announceMent} headerClass="bg-blue-300" rightBlock={(
       <>Right</>
     )}>
       <TwixtToolName name="Twixt UI React App" />
