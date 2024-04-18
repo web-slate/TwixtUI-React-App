35a956b96ba8b466bba95a0ceb6d1d9640c02e0f
Venkat.R <ramsunvtech@gmail.com>
2024-04-18 21:55:13 +0800

    append heading

--- src/examples/Heading.js
+++ src/examples/Heading.js
@@ -12,6 +12,11 @@ export default function Heading() {
   return (
     <div style={headingContainerStyle}>
       <TwixtHeading type="h1">Heading 1 goes here</TwixtHeading>
+      <TwixtHeading type="h2">Heading 2 goes here</TwixtHeading>
+      <TwixtHeading type="h3">Heading 3 goes here</TwixtHeading>
+      <TwixtHeading type="h4">Heading 4 goes here</TwixtHeading>
+      <TwixtHeading type="h5">Heading 5 goes here</TwixtHeading>
+      <TwixtHeading type="h6">Heading 6 goes here</TwixtHeading>
     </div>
   );
 }
